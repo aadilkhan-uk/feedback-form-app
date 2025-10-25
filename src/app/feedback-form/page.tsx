a506bc3ac38f0142e331336d625042eaa20e5e97
aadilkhan-uk <aadil.khan6425@gmail.com>
2025-10-25 12:37:45 +0100

    Pull survery from server and display

--- src/app/feedback-form/page.tsx
+++ src/app/feedback-form/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import { useState } from "react";
 import { useSurvey } from "../../hooks/useSurvey";
 import { LoadingState, ErrorState, SurveyDebug } from "../_components";
 import { FeedbackFormLayout } from "../_components/layouts/FeedbackFormLayout";
+import {
+  Button,
+  RatingQuestionType,
+  TextQuestionType,
+} from "../_components/theme";
 
 export default function FeedbackFormPage() {
   const { survey, isLoading, error } = useSurvey();
+  const [formResponses, setFormResponses] = useState<
+    Record<string, string | number>
+  >({});
+
+  const handleRatingChange = (questionKey: string, rating: number) => {
+    setFormResponses((prev) => ({
+      ...prev,
+      [questionKey]: rating,
+    }));
+  };
+
+  const handleTextChange = (questionKey: string, text: string) => {
+    setFormResponses((prev) => ({
+      ...prev,
+      [questionKey]: text,
+    }));
+  };
 
   if (isLoading) {
     return (
@@ -23,9 +46,49 @@ export default function FeedbackFormPage() {
     );
   }
 
+  if (!survey) {
+    return (
+      <FeedbackFormLayout>
+        <ErrorState error={{ message: "No survey data available" }} />
+      </FeedbackFormLayout>
+    );
+  }
+
   return (
-    <FeedbackFormLayout title={survey?.title}>
-      <SurveyDebug survey={survey!} />
+    <FeedbackFormLayout title={survey.title}>
+      <div className="space-y-8">
+        {survey.questions.map((question) => {
+          if (question.type === "rating") {
+            return (
+              <div key={question.key} className="pb-6">
+                <RatingQuestionType
+                  key={question.key}
+                  label={question.label}
+                  onRatingChange={(rating) =>
+                    handleRatingChange(question.key, rating)
+                  }
+                />
+              </div>
+            );
+          }
+
+          if (question.type === "text") {
+            return (
+              <div key={question.key} className="pb-6">
+                <TextQuestionType
+                  key={question.key}
+                  className="mb-6"
+                  label={question.label}
+                  onTextChange={(text) => handleTextChange(question.key, text)}
+                />
+              </div>
+            );
+          }
+
+          return null;
+        })}
+        <Button>Submit</Button>
+      </div>
     </FeedbackFormLayout>
   );
 }
